a418636d32e55f34af127b6277a0f3559fbc4ae4
Rafael <rafaelbitencourrt@gmail.com>
2022-02-06 08:24:40 -0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 let weather = {
   apiKey: "53de8758770c4f74c302a0a05a8cd1f1",
+  fecthWeather: function () {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  },
 };

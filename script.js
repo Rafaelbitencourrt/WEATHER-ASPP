43b3337b094dfa33cf83ad3a405abacfa80580be
Rafael <rafaelbitencourrt@gmail.com>
2022-02-09 08:57:41 -0300

    update

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 let weather = {
   apiKey: "53de8758770c4f74c302a0a05a8cd1f1",
-  fecthWeather: function () {
+  fecthWeather: function (city) {
     fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
         city +
         "&units=metric&appid=" +
-        apiKey
+        this.apiKey
     )
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather;
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "O tempo em " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + "@2x.png";
   },
 };

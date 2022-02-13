2a0c5b06cc9d771bf8211a531ee0bdc675857032
Rafael <rafaelbitencourrt@gmail.com>
2022-02-13 08:23:17 -0300

    ok

--- script.js
+++ script.js
@@ -18,6 +18,11 @@ let weather = {
     console.log(name, icon, description, temp, humidity, speed);
     document.querySelector(".city").innerText = "O tempo em " + name;
     document.querySelector(".icon").src =
-      "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText = "Umidade:" + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Velocidade do tempo" + speed + "km/h";
   },
 };
